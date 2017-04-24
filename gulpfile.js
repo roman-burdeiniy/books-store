24efcbbbec95ceb1da3a9fb17353f519a0802a05
r.burdeiniy <roman.burdeiniy@gmail.com>
2017-04-24 14:18:36 +0300

    api endpoint updated

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,6 @@ var runSequence = require('run-sequence');
 
 const BUILD_PATH = './dist';
 const SOURCE_PATH = './src';
-const API_SOURCE_PATH = SOURCE_PATH + '/shop-api';
 const BACKEND_SOURCE_PATH = SOURCE_PATH + '/shop-backend';
 const FRONTEND_SOURCE_PATH = SOURCE_PATH + '/shop-frontend';
 
@@ -40,7 +39,7 @@ gulp.task('copy-frontend-resources', function () {
 });
 
 gulp.task('clean-dist', function () {
-    return gulp.src(['./dist/*', '!' + BUILD_PATH + '/shop-api'], {read: false})
+    return gulp.src(['./dist/*'], {read: false})
         .pipe(clean());
 });
 
@@ -49,51 +48,11 @@ gulp.task('clean-frontend-source-dist', function () {
         .pipe(clean());
 });
 
-gulp.task('clean-dist-api', function () {
-    return gulp.src('./dist/shop-api/*', {read: false})
-        .pipe(clean());
-});
-
-gulp.task('populate-test-db', function() {
-    return run('mongo ./data/scripts/populate-db.js')
-        .exec()
-        .pipe(gulp.dest('./data/output'))
-})
-
-gulp.task('populate-prod-db', function() {
-    return run('mongo ./data/scripts/populate-prod-db.js')
-        .exec()
-        .pipe(gulp.dest('./data/output'))
-})
-
-gulp.task('populate-dev-db', function() {
-    return run('mongo ./data/scripts/populate-local-db.js')
-        .exec()
-        .pipe(gulp.dest('./data/output'))
-})
-
-gulp.task('build-api', ['clean-dist-api'], function() {
-    gulp.src([API_SOURCE_PATH + '/views/*'])
-        .pipe(gulp.dest(BUILD_PATH + '/shop-api/views'));
-    gulp.src([API_SOURCE_PATH + '/img/**/*'])
-        .pipe(gulp.dest(BUILD_PATH + '/shop-api/img'));
-    gulp.src([API_SOURCE_PATH + '/public/**/*'])
-        .pipe(gulp.dest(BUILD_PATH + '/shop-api/public'));
-    return gulp.src([API_SOURCE_PATH + '/**/*.js'])
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['es2015', "stage-2"]
-        }))
-        .pipe(sourcemaps.write('./maps',{includeContent:true, sourceRoot:'../../src/shop-api'}))
-        .pipe(gulp.dest(BUILD_PATH + '/shop-api'));
-});
-
 gulp.task('build-frontend-dev', function(done) {
     exec('npm --prefix ./src/shop-frontend run build-dev', function(error, stdout, stderr) {
         if(error) {
             console.log(error, stderr);
         }
-        console.log("frontend built");
         gulp.src([FRONTEND_SOURCE_PATH + '/build/**/*'])
             .pipe(gulp.dest(BACKEND_SOURCE_PATH + '/public'));
         done();
--- src/shop-backend/config/env/prod.js
+++ src/shop-backend/config/env/prod.js
@@ -1,4 +1,4 @@
 /**
  * Created by roman_b on 3/14/2017.
  */
-export const apiEndpoint = "mongodb://heroku_6tpwlxl9:Gosha777@ds111461.mlab.com:11461/heroku_6tpwlxl9";
+export const apiEndpoint = "https://book-case-api.herokuapp.com/shop-api";
